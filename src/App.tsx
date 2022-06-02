79551710db98709c89a857b145bdc2c737f7e093
Wickramaranga Abeygunawardhana <w@umstek.tk>
2022-06-02 20:11:32 +0530

    Add project files

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="App h-96 w-96 bg-red-50"></div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{t,j}s{,x}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
